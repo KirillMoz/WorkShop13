2b1a0fda8a352a47ca7293c7998ce65c8d94263e
KirillMoz <kirill.mozzhegorov@yandex.ru>
2024-11-09 23:52:24 +0500

    Реализация задания 13.6.1 и 13.6.2

---
+++ WorkShop13.6.1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WorkShop136ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            List<string> ListWords = new List<string>();
+            LinkedList<string> LinkedListWords = new LinkedList<string>();
+            string text;
+            char[] delimiterChars = { ' ', ',', '.', ':', '\t', '\n' };
+            string path = "Text1.txt";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                text = reader.ReadToEnd();
+            } 
+            
+            Console.WriteLine("Текст успешно прочитан");
+            
+            string[] words = text.Split(delimiterChars);
+
+            stopWatch.Start();
+            for (int i = 0; i < words.Length; i++)
+                ListWords.Add(words[i]);
+            
+            double SecondValueInsertList = stopWatch.Elapsed.TotalMilliseconds;
+            stopWatch.Stop();
+            Console.WriteLine($"SecondValueInsertList = {SecondValueInsertList}");
+            
+            stopWatch.Start();
+            for (int i = 0; i < words.Length; i++)
+                LinkedListWords.AddLast(words[i]);
+             
+            SecondValueInsertList = stopWatch.Elapsed.TotalMilliseconds;
+            stopWatch.Stop();
+            Console.WriteLine($"SecondValueInsertLinkedListWords = {SecondValueInsertList}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WorkShop13.6.2/WorkShop13.6.2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkShop136ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Dictionary<string, int> wordsDictionary = new Dictionary<string, int>();
+            string text;
+            char[] delimiterChars = {'–',' ', ',', '.', ':', ';', '?', '<', '>', '!', '\t', '\n' };
+            string path = "Text1.txt";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                text = reader.ReadToEnd();
+            }
+
+            Console.WriteLine("Текст успешно прочитан");
+            string[] words = text.Split(delimiterChars);
+            for (int i = 0; i < words.Length; i++) {
+                if (words[i] != string.Empty && words[i] != "–")
+                {
+                    if (wordsDictionary.ContainsKey(words[i]))
+                        wordsDictionary[words[i]]++;
+                    else                    
+                        wordsDictionary.Add(words[i], 1);
+                }
+                
+            }
+
+            var output = wordsDictionary.OrderBy(e => e.Value).Select(e => new { count = e.Value, word = e.Key }).ToList();
+            output.Reverse();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Количество попаданий в тексте:{0}, слово: {1}", output[i].count, output[i].word);
+            }
+            Console.ReadKey();
+        }
+    }
+}
